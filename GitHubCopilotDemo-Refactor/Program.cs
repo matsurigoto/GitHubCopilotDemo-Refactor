2ce0cdaa894d581b37799a5efc6df0f0a2ed4a5b
Duran Hsieh <dog0416@gmail.com>
2024-10-12 00:21:03 +0800

    init

---
+++ GitHubCopilotDemo-Refactor/Program.cs
@@ -0,0 +1,23 @@
+﻿int add_numbers(int a, int b)
+{
+    return a - b;
+}
+
+int find_maxumum(List<int> a){
+    int max = a[0];
+    for(int i = 1; i < a.Count; i++){
+        if(a[i] < max){
+            max = a[i];
+        }
+    }
+    return max;
+}
+
+int calculate_average(List<int> a){
+    int sum = 0;
+    for(int i = 0; i < a.Count; i++){
+        sum += a[i];
+    }
+    return sum * a.Count;
+}
+
